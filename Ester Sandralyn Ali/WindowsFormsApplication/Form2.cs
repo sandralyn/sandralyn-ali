f09855f9b7254fa0348999406d7e9df4f05f7b5b
johnas07 <johnas_esco07@yahoo.com>
2014-07-04 14:13:59 -0700

    Assignment
    Free elective 1

---
+++ Ester Sandralyn Ali/WindowsFormsApplication/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class Form2 : Form
+    {
+        public Form1 form1 = new Form1();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form1.button1.Text = (int.Parse(form1.button1.Text) + int.Parse(textBox1.Text)).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
